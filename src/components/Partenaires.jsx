d69bd9b674f492640b121fe539885b94a5b6a70c
Enzo Santamaria <enzo@roomba.io>
2022-02-16 15:41:18 +0100

    Partenaire component

---
+++ src/components/Partenaires.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Partenaires = ({ data }) => {
+  return (
+    <section className=" bg-kaki">
+      <div className="flex flex-col mx-4  items-center pt-14 pb-14 ">
+        <h3 className=" text-cream mb-5 font-medium text-2xl text-center">
+          Nos Partenaires
+        </h3>
+        <div className="sm:flex flex-row w-full  ">
+          {data.map(({ attributes }, index) => (
+            <img
+              key={index}
+              src={process.env.REACT_APP_URI + attributes.url}
+              alt="Logo de nos partenaires"
+              className="mx-auto  w-1/2 sm:w-1/5 md:w-1/6 lg:w-40"
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default React.memo(Partenaires);
